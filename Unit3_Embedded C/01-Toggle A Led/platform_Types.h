bbf3cec98a3e1bd0373f1470af0ea7f529af2840
Abdelrhman Hamdy <abdelrhmanhamdy023@gmail.com>
2024-08-06 16:38:28 +0300

    updates on toggle a led project

--- Unit3_Embedded C/01-Toggle A Led/platform_Types.h
+++ Unit3_Embedded C/01-Toggle A Led/platform_Types.h
@@ -1,28 +1,35 @@
+/*
+ * platform_types.h
+ *
+ *  Created on: Aug 6, 2024
+ *      Author: AbdoHamdy
+ */
 
 #ifndef PLATFORM_TYPES_H_
 #define PLATFORM_TYPES_H_
 
 typedef unsigned char      uint8_t;
 typedef signed char        sint8_t;
-typedef volatile uint8     vuint8_t;
-typedef volatile sint8     vsint8_t;
+typedef volatile uint8_t   vuint8_t;
+typedef volatile sint8_t   vsint8_t;
 
 typedef unsigned short     uint16_t;
 typedef signed short       sint16_t;
-typedef volatile uint16    vuint16_t;
-typedef volatile sint16    vSint16_t;
+typedef volatile uint16_t  vuint16_t;
+typedef volatile sint16_t  vSint16_t;
 
 typedef unsigned long int  uint32_t;
 typedef signed long int    sint32_t;
-typedef volatile uint32    vuint32_t;
-typedef volatile sint32    vsint32_t;
+typedef volatile uint32_t  vuint32_t;
+typedef volatile sint32_t  vsint32_t;
 
 typedef unsigned long long uint64_t;
 typedef signed long long   sint64_t;
-typedef volatile uint64    vuint64_t;
-typedef volatile sint64    vsint64_t;
+typedef volatile uint64_t  vuint64_t;
+typedef volatile sint64_t  vsint64_t;
 
 typedef float              f32_t;
 typedef double             f64_t;
 
-#endif
+
+#endif /* PLATFORM_TYPES_H_ */
