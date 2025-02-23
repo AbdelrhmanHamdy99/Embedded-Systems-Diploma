63feb9b4867ab6b210a8c99da9861034d58e1996
Abdelrhman Hamdy <104307082+AbdelrhmanHamdy99@users.noreply.github.com>
2025-02-23 15:16:06 +0200

    Delete Unit7_MCU_Essential_Peripherals/Lesson4-EXTI/Lab directory

--- Unit7_MCU_Essential_Peripherals/Lesson4-EXTI/Lab/STM32CubeIDE Full Project/Debug/STM32F103C6Tx_Drivers/GPIO/stm32f103c6_GPIO_Driver.d
+++
@@ -1,8 +0,0 @@
-STM32F103C6Tx_Drivers/GPIO/stm32f103c6_GPIO_Driver.o: \
- ../STM32F103C6Tx_Drivers/GPIO/stm32f103c6_GPIO_Driver.c \
- D:/courses/embeded\ systems/Embeded\ System\ Diploma/workspace/stm32/Unit7_Lesson4_2/STM32F103C6Tx_Drivers/Inc/stm32f103c6_GPIO_Driver.h \
- D:/courses/embeded\ systems/Embeded\ System\ Diploma/workspace/stm32/Unit7_Lesson4_2/STM32F103C6Tx_Drivers/Inc/stm32f103x6.h
-
-D:/courses/embeded\ systems/Embeded\ System\ Diploma/workspace/stm32/Unit7_Lesson4_2/STM32F103C6Tx_Drivers/Inc/stm32f103c6_GPIO_Driver.h:
-
-D:/courses/embeded\ systems/Embeded\ System\ Diploma/workspace/stm32/Unit7_Lesson4_2/STM32F103C6Tx_Drivers/Inc/stm32f103x6.h:
